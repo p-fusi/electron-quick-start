8b1cb7c94058f0601696047bbe98b742822da082
p.fusi <p.fusi@ambrogio.com>
2020-02-14 12:03:30 +0100

    wip

--- index.js
+++
@@ -1,7 +0,0 @@
-function basicNotification() {
-    const notification = {
-        title: 'Basic Notification',
-        body: 'This is a basic Electron notification'
-    }
-    const myNotification = new window.Notification(notification.title, notification);
-}
